eaa1abb12bd52c76ef3c610593227f6a9a0446d3
Diana Mejías Hernández <85594834+dmejias08@users.noreply.github.com>
2024-03-14 00:36:54 -0600

    create alu for vectors and test benches. Add scalar alu

--- alu_element_vec_tb.sv
+++ alu_element_vec_tb.sv
@@ -8,7 +8,7 @@ module alu_element_vec_tb;
   // Inputs
   logic [ELEMENT_SIZE-1:0] elementA;
   logic [ELEMENT_SIZE-1:0] elementB;
-  logic [2:0] opcode;
+  logic [2:0] sel;
   
   // Outputs
   logic [ELEMENT_SIZE-1:0] result;
@@ -18,19 +18,60 @@ module alu_element_vec_tb;
   alu_element_vec #(ELEMENT_SIZE) uut (
     .elementA(elementA),
     .elementB(elementB),
-    .opcode(opcode),
+    .sel(sel),
     .result(result)
   );
   
 // Stimulus generation
   initial begin
     // Test case 1: Addition
-    elementA = 16'd8;
-    elementB = 16'd5;
-	expected_result = 16'd13;
-    opcode = 3'b000;
-	 #10;
-	assert(result == expected_result) else $fatal("Test failed");
+        elementA = 16'd8;
+        elementB = 16'd5;
+        sel = 3'b000;
+        #10;
+        assert(result == 16'd13) else $fatal("Test failed for addition");
+
+        // Test case 2: Subtraction
+        elementA = 16'd10;
+        elementB = 16'd3;
+        sel = 3'b001;
+        #10;
+        assert(result == 16'd7) else $fatal("Test failed for subtraction");
+
+        // Test case 3: Multiplication
+        elementA = 16'd5;
+        elementB = 16'd6;
+        sel = 3'b010;
+        #10;
+        assert(result == 16'd30) else $fatal("Test failed for multiplication");
+
+        // Test case 4: Arithmetic right shift
+        elementA = 16'd32;
+        elementB = 16'd2;
+        sel = 3'b011;
+        #10;
+        assert(result == 16'd8) else $fatal("Test failed for arithmetic right shift");
+
+        // Test case 5: Logical right shift
+        elementA = 16'd32;
+        elementB = 16'd2;
+        sel = 3'b100;
+        #10;
+        assert(result == 16'd8) else $fatal("Test failed for logical right shift");
+
+        // Test case 6: Logical left shift
+        elementA = 16'd4;
+        elementB = 16'd2;
+        sel = 3'b101;
+        #10;
+        assert(result == 16'd16) else $fatal("Test failed for logical left shift");
+
+        // Test case 7: Bitwise AND
+        elementA = 16'd15;
+        elementB = 16'd10;
+        sel = 3'b110;
+        #10;
+        assert(result == 16'd10) else $fatal("Test failed for bitwise AND");
 
   end
   
